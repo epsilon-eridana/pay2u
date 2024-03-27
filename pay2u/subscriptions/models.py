b9bcbec5391bb6fb37d618631f5cfc14cc870310
andrey <mist3s@github.com>
2024-03-27 18:46:03 +0200

    Добавить модель оценки сервисов.

--- pay2u/subscriptions/models.py
+++ pay2u/subscriptions/models.py
@@ -1,16 +1,64 @@
 from django.contrib.auth import get_user_model
+from django.core.validators import MaxValueValidator, MinValueValidator
 from django.db import models
 from djmoney.models.fields import MoneyField
+
+from .constants import MAX_LIMIT_VALUE, MIN_LIMIT_VALUE
 
 User = get_user_model()
 
 
-class Сategory(models.Model):
+class Category(models.Model):
     title = models.CharField(max_length=200)
     slug = models.SlugField(unique=True)
 
     def __str__(self):
         return self.title
+
+
+class Evaluation(models.Model):
+    """Модель оценки."""
+    service = models.ForeignKey(
+        'Service',
+        on_delete=models.CASCADE,
+        related_name='evaluations',
+        verbose_name='Сервис',
+        help_text='Нужно выбрать сервис.'
+    )
+    user = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='evaluations',
+        verbose_name='Пользователь',
+        help_text='Пользователь который оценивает сервис.'
+    )
+    score = models.PositiveSmallIntegerField(
+        validators=[
+            MaxValueValidator(
+                limit_value=MAX_LIMIT_VALUE,
+                message=f'Оценка больше {MAX_LIMIT_VALUE}.'
+            ),
+            MinValueValidator(
+                limit_value=MIN_LIMIT_VALUE,
+                message=f'Оценка меньше {MIN_LIMIT_VALUE}.'
+            )
+        ],
+        verbose_name='Оценка',
+        help_text=(f'Оцените сервис, в диапазоне от {MIN_LIMIT_VALUE}'
+                   f'до {MAX_LIMIT_VALUE}')
+    )
+
+    class Meta:
+        ordering = ('-pk',)
+        verbose_name = 'Оценка'
+        verbose_name_plural = 'Оценки'
+        constraints = [models.UniqueConstraint(
+            fields=['service', 'user'],
+            name='unique_evaluation'
+        )]
+
+    def __str__(self):
+        return f"{self.service} - {self.user} / {self.score}"
 
 
 class Service(models.Model):
@@ -37,7 +85,7 @@ class Subscription(models.Model):
         related_name="subscription", blank=True, null=True
     )
     category = models.ForeignKey(
-        Сategory, on_delete=models.SET_NULL,
+        Category, on_delete=models.SET_NULL,
         related_name="subscription", blank=True, null=True
     )
     title = models.CharField(max_length=200)
