45903f3e4543eba211e5fb7673a58344f79eae95
Андрей Иванов <ivanov.andrevv@gmail.com>
2024-04-01 13:55:46 +0200

    Написать модель сервиса подписки, категории и тегов.

--- pay2u/subscriptions/models.py
+++ pay2u/subscriptions/models.py
@@ -1,140 +1,88 @@
 from django.contrib.auth import get_user_model
-from django.core.validators import MaxValueValidator, MinValueValidator
 from django.db import models
-
-from .constants import MAX_LIMIT_VALUE, MIN_LIMIT_VALUE
 
 User = get_user_model()
 
 
 class Category(models.Model):
-    title = models.CharField(max_length=200)
-    slug = models.SlugField(unique=True)
-
-    def __str__(self):
-        return self.title
-
-
-class Evaluation(models.Model):
-    """Модель оценки."""
-    service = models.ForeignKey(
-        'Service',
-        on_delete=models.CASCADE,
-        related_name='evaluations',
-        verbose_name='Сервис',
-        help_text='Нужно выбрать сервис.'
+    """Модель категории."""
+    name = models.CharField(
+        max_length=200,
+        unique=True,
+        verbose_name='Название'
     )
-    user = models.ForeignKey(
-        User,
-        on_delete=models.CASCADE,
-        related_name='evaluations',
-        verbose_name='Пользователь',
-        help_text='Пользователь который оценивает сервис.'
-    )
-    score = models.PositiveSmallIntegerField(
-        validators=[
-            MaxValueValidator(
-                limit_value=MAX_LIMIT_VALUE,
-                message=f'Оценка больше {MAX_LIMIT_VALUE}.'
-            ),
-            MinValueValidator(
-                limit_value=MIN_LIMIT_VALUE,
-                message=f'Оценка меньше {MIN_LIMIT_VALUE}.'
-            )
-        ],
-        verbose_name='Оценка',
-        help_text=(f'Оцените сервис, в диапазоне от {MIN_LIMIT_VALUE}'
-                   f'до {MAX_LIMIT_VALUE}')
+    slug = models.SlugField(
+        unique=True,
+        max_length=200,
+        verbose_name='Slug'
     )
 
     class Meta:
-        ordering = ('-pk',)
-        verbose_name = 'Оценка'
-        verbose_name_plural = 'Оценки'
-        constraints = [models.UniqueConstraint(
-            fields=['service', 'user'],
-            name='unique_evaluation'
-        )]
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
 
     def __str__(self):
-        return f"{self.service} - {self.user} / {self.score}"
+        return self.name
+
+
+class Tag(models.Model):
+    """Модель тега."""
+    name = models.CharField(
+        max_length=200,
+        unique=True,
+        verbose_name='Название',
+    )
+    slug = models.SlugField(
+        max_length=200,
+        unique=True,
+        verbose_name='Слаг',
+    )
+    color = models.CharField(
+        max_length=7,
+        verbose_name='Цвет',
+    )
+
+    class Meta:
+        verbose_name = 'Тег'
+        verbose_name_plural = 'Теги'
+
+    def __str__(self):
+        return self.name
 
 
 class Service(models.Model):
-    title = models.CharField(max_length=200)
-    description = models.TextField()
-
-    def __str__(self):
-        return self.title
-
-
-class Payment(models.Model):
-    amount = models.DecimalField(
-        max_digits=14, decimal_places=2
+    """Модель сервиса подписки."""
+    name = models.CharField(
+        max_length=200,
+        verbose_name='Название'
     )
-    date = models.DateTimeField(
-        'Дата оплаты', auto_now_add=True, db_index=True
+    short_description = models.CharField(
+        max_length=200,
+        verbose_name='Краткое описание'
     )
-
-
-class Subscription(models.Model):
-    service = models.ForeignKey(
-        Service, on_delete=models.SET_NULL,
-        related_name="subscription", blank=True, null=True
+    description = models.TextField(
+        max_length=1000,
+        verbose_name='Описание'
     )
     category = models.ForeignKey(
-        Category, on_delete=models.SET_NULL,
-        related_name="subscription", blank=True, null=True
+        Category,
+        on_delete=models,
+        related_name='services',
+        verbose_name='Категория'
     )
-    title = models.CharField(max_length=200)
-    description = models.TextField()
-    price = models.DecimalField(
-        max_digits=14, decimal_places=2
+    icon = models.ImageField(
+        upload_to='services/images/',
+        verbose_name='Иконка'
     )
-    period = ...
-
-    def __str__(self):
-        return self.title
-
-
-class Follow(models.Model):
-    user = models.ForeignKey(
-        User, on_delete=models.CASCADE, related_name="follower"  # !
+    tags = models.ManyToManyField(
+        Tag,
+        related_name='services',
+        verbose_name='Теги'
     )
-    subscription = models.ForeignKey(
-        Subscription, on_delete=models.CASCADE, related_name="subscription"  # !
-    )
-    payment = models.ForeignKey(
-        Payment, on_delete=models.CASCADE, related_name="follower"  # !
-    )
-    date_subscription = models.DateTimeField(
-        'Дата начала', auto_now_add=True, db_index=True
-    )
-    date_payment = models.DateTimeField(
-        'Дата списания', auto_now_add=True, db_index=True
-    )
-    status = ...
 
     class Meta:
-        constraints = [
-            models.UniqueConstraint(
-                fields=["user", "subscription"], name="unique_follow"
-            )
-        ]
+        verbose_name = 'Сервис'
+        verbose_name_plural = 'Сервисы'
 
-
-class Cashback(models.Model):
-    user = models.ForeignKey(
-        User, on_delete=models.CASCADE, related_name="follower"  # !
-    )
-    payment = models.ForeignKey(
-        Payment, on_delete=models.CASCADE, related_name="follower"  # !
-    )
-    amount = models.DecimalField(
-        max_digits=14, decimal_places=2
-    )
-    status = ...
-    balance = models.DecimalField(
-        max_digits=14, decimal_places=2
-    )
-    interest_rate = ...
+    def __str__(self):
+        return self.name
