8aec6629181054d57a84760bb806afd53f5d1e3b
Андрей Иванов <ivanov.andrevv@gmail.com>
2024-04-01 16:15:36 +0200

    Настроить админ панель.

--- pay2u/subscriptions/admin.py
+++ pay2u/subscriptions/admin.py
@@ -1,3 +1,44 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import (
+    Service, Category, Rate, Option, ServiceRate, ServiceOptions
+)
+
+
+class ServiceRateInline(admin.TabularInline):
+    model = ServiceRate
+    extra = 1
+
+
+class ServiceOptionsInline(admin.TabularInline):
+    model = ServiceOptions
+    extra = 1
+
+
+@admin.register(Service)
+class ServiceAdmin(admin.ModelAdmin):
+    list_display = (
+        'id', 'name', 'category', 'short_description'
+    )
+    inlines = [ServiceRateInline, ServiceOptionsInline]
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = (
+        'id', 'name', 'slug'
+    )
+
+
+@admin.register(Option)
+class OptionAdmin(admin.ModelAdmin):
+    list_display = (
+        'id', 'name'
+    )
+
+
+@admin.register(Rate)
+class RateAdmin(admin.ModelAdmin):
+    list_display = (
+        'id', 'name', 'price', 'duration', 'cashback'
+    )
