a8f80250267d7b85847ce6a80ceb5ea0c9552ecb
Андрей Иванов <ivanov.andrevv@gmail.com>
2024-04-02 15:07:40 +0200

    Перенес модель пользователя.

--- pay2u/services/models.py
+++ pay2u/services/models.py
@@ -1,8 +1,6 @@
-from django.contrib.auth import get_user_model
 from django.core.validators import MaxValueValidator, MinValueValidator
 from django.db import models
 
-User = get_user_model()
 MAX_LIMIT_VALUE = 10000
 MIN_LIMIT_VALUE = 1
 MAX_LIMIT_VALUE_CASHBACK = 100
