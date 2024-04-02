6643b4dc928325667eb1dfab64b9af6d8c8ee6f8
Андрей Иванов <ivanov.andrevv@gmail.com>
2024-04-02 15:27:39 +0200

    Исправить опечатку.

--- pay2u/subscriptions/models.py
+++ pay2u/subscriptions/models.py
@@ -2,7 +2,7 @@ from django.contrib.auth import get_user_model
 from django.core.validators import MaxValueValidator, MinValueValidator
 from django.db import models
 
-from .services.models import Rate
+from services.models import Rate
 
 User = get_user_model()
 MAX_LIMIT_VALUE_PRICE = 999999
