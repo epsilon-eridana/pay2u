1e3c82e714bb6955163e8cb16b8cd21168bdb6d9
Андрей Иванов <ivanov.andrevv@gmail.com>
2024-04-02 11:37:17 +0200

    Добавить приложение подписок.

--- pay2u/services/apps.py
+++ pay2u/services/apps.py
@@ -1,7 +1,7 @@
 from django.apps import AppConfig
 
 
-class SubscriptionsConfig(AppConfig):
+class ServicesConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "services"
     verbose_name = "Сервисы"
