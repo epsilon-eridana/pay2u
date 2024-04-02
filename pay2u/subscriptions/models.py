4b4be7f5219f931cbb5bba0ac2ce7233de10f0a4
Андрей Иванов <ivanov.andrevv@gmail.com>
2024-04-02 15:17:07 +0200

    Добавить модель покупок.

--- pay2u/subscriptions/models.py
+++ pay2u/subscriptions/models.py
@@ -1,9 +1,13 @@
 from django.contrib.auth import get_user_model
+from django.core.validators import MaxValueValidator, MinValueValidator
 from django.db import models
 
 from .services.models import Rate
 
 User = get_user_model()
+MAX_LIMIT_VALUE_PRICE = 999999
+MIN_LIMIT_VALUE_PRICE = 0
+DEFAULT_CASHBACK = 0
 
 
 class Subscription(models.Model):
@@ -36,3 +40,60 @@ class Subscription(models.Model):
 
     def __str__(self):
         return f'{self.user} / {self.rate.name}'
+
+
+class Payment(models.Model):
+    """Модель покупок."""
+    subscription = models.OneToOneField(
+        Subscription,
+        on_delete=models.CASCADE,
+        related_name='payment',
+        verbose_name='Подписка'
+    )
+    price = models.DecimalField(
+        max_digits=14,
+        decimal_places=2,
+        verbose_name='Цена',
+        validators=[
+            MaxValueValidator(
+                limit_value=MAX_LIMIT_VALUE_PRICE,
+                message=f'Значение должно быть меньше '
+                        f'{MAX_LIMIT_VALUE_PRICE}.'
+            ),
+            MinValueValidator(
+                limit_value=MIN_LIMIT_VALUE_PRICE,
+                message=f'Значение должно быть больше '
+                        f'{MIN_LIMIT_VALUE_PRICE}.'
+            )
+        ]
+    )
+    cashback = models.DecimalField(
+        max_digits=14,
+        decimal_places=2,
+        verbose_name='Кэшбэк',
+        validators=[
+            MaxValueValidator(
+                limit_value=MAX_LIMIT_VALUE_PRICE,
+                message=f'Значение должно быть меньше '
+                        f'{MAX_LIMIT_VALUE_PRICE}.'
+            ),
+            MinValueValidator(
+                limit_value=MIN_LIMIT_VALUE_PRICE,
+                message=f'Значение должно быть больше '
+                        f'{MIN_LIMIT_VALUE_PRICE}.'
+            )
+        ],
+        default=DEFAULT_CASHBACK
+    )
+    date = models.DateTimeField(
+        verbose_name='Дата',
+        auto_now_add=True,
+        db_index=True
+    )
+
+    class Meta:
+        verbose_name = 'Покупка'
+        verbose_name_plural = 'Покупки'
+
+    def __str__(self):
+        return f'{self.subscription} / {self.price}'
