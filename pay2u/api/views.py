a8a490a92dfce3638b6681ceeeb06d02f67d0d99
Mist3s <ivanov.andrevv@gmail.com>
2024-04-06 21:32:31 +0200

    Написать вьюсет для сервисов.

--- pay2u/api/views.py
+++ pay2u/api/views.py
@@ -1,6 +1,10 @@
-from rest_framework import status, viewsets
+from rest_framework import status, viewsets, mixins
 
-from .serializers import CategoryListSerializer, CategoryDetailSerializer
+from .serializers import (
+    CategoryListSerializer,
+    CategoryDetailSerializer,
+    ServiceSerializer
+)
 from services.models import Category, Service
 
 
@@ -14,3 +18,15 @@ class CategoryViewSet(viewsets.ReadOnlyModelViewSet):
         if self.action == 'retrieve':
             return CategoryDetailSerializer
         return self.serializer_class
+
+
+class ServiceViewSet(
+    mixins.RetrieveModelMixin,viewsets.GenericViewSet
+):
+    queryset = Service.objects.all().prefetch_related(
+        'tags',
+        'rates',
+        'options',
+        'images'
+    )
+    serializer_class = ServiceSerializer
