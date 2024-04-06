eeccd7d76b8a15ba47cb639748011556fda2fe66
Mist3s <ivanov.andrevv@gmail.com>
2024-04-06 21:33:28 +0200

    Зарегистрировать эндпоинт сервиса в роутер.

--- pay2u/api/urls.py
+++ pay2u/api/urls.py
@@ -1,12 +1,13 @@
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 
-from .views import CategoryViewSet
+from .views import CategoryViewSet, ServiceViewSet
 
 app_name = 'api'
 
 router_v1 = DefaultRouter()
 router_v1.register('categories', CategoryViewSet, basename='categories')
+router_v1.register('services', ServiceViewSet, basename='services')
 
 urlpatterns = [
     path('v1/', include(router_v1.urls)),
