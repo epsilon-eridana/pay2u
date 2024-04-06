b2577bd6fdb6db31b508287f2c38abb477710cbb
andrey <mist3s@github.com>
2024-04-06 11:23:37 +0200

    Зарегистрировать модель промокода в админке.

--- pay2u/promocodes/admin.py
+++ pay2u/promocodes/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import PromoCode
+
+
+@admin.register(PromoCode)
+class PromoCodeAdmin(admin.ModelAdmin):
+    list_display = (
+        'id',
+        'name',
+        'service',
+        'user',
+        'key'
+    )
