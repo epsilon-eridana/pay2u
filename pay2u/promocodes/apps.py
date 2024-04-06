29a3d0f748fdac5073256050f7d5050688991353
andrey <mist3s@github.com>
2024-04-06 11:02:15 +0200

    Добавить русское название приложения.

--- pay2u/promocodes/apps.py
+++ pay2u/promocodes/apps.py
@@ -4,3 +4,4 @@ from django.apps import AppConfig
 class PromocodesConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'promocodes'
+    verbose_name = 'Промокоды'
