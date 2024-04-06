8636dcea0572b2c64ec1f2a96dfba19561df6a87
andrey <mist3s@github.com>
2024-04-06 15:07:45 +0200

    Дописать модель сервиса на странице категории.

--- pay2u/api/serializers.py
+++ pay2u/api/serializers.py
@@ -1,19 +1,54 @@
 from rest_framework import serializers
 
-from services.models import Category, Service
+from services.models import Category, Service, Tag
 
 
-class ServiceSerializer(serializers.ModelSerializer):
+class TagSerializer(serializers.ModelSerializer):
+    """Сериализатор для модели Таг."""
+    class Meta:
+        model = Tag
+        fields = ('name', 'color', 'slug')
+
+
+class ServiceCategorySerializer(serializers.ModelSerializer):
     """Сериализатор для модели Сервиса."""
+    tags = TagSerializer(many=True)
+    cashback = serializers.SerializerMethodField()
+    price_month = serializers.SerializerMethodField()
+    is_followed = serializers.SerializerMethodField()
+
     class Meta:
         model = Service
         fields = (
             'name',
-            'short_description',
-            'description',
             'icon',
-            'url'
+            'tags',
+            'cashback',
+            'price_month',
+            'is_followed'
         )
+
+    def get_cashback(self, obj):
+        rates_with_cashback = obj.rates.all()
+        if rates_with_cashback:
+            max_cashback_rate = max(
+                rates_with_cashback, key=lambda rate: rate.cashback
+            )
+            return max_cashback_rate.cashback
+        return None
+
+    def get_price_month(self, obj):
+        rates_with_price = obj.rates.all()
+        if rates_with_price:
+            min_cashback_rate = min(
+                rates_with_price, key=lambda rate: rate.price_month
+            )
+            return min_cashback_rate.price_month
+        return None
+
+    def get_is_followed(self, obj):
+        # Временная затычка, нужно написать менеджер модели.
+        return False
 
 
 class CategoryListSerializer(serializers.ModelSerializer):
@@ -28,7 +63,7 @@ class CategoryListSerializer(serializers.ModelSerializer):
 
 class CategoryDetailSerializer(serializers.ModelSerializer):
     """Сериализатор для детальной информации о категории."""
-    services = ServiceSerializer(
+    services = ServiceCategorySerializer(
         many=True,
         read_only=True
     )
