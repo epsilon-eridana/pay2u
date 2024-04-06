c760f3372eb770fa00874244916a2fe72d933b95
andrey <mist3s@github.com>
2024-04-06 14:04:34 +0200

    Написать сериализатор для модели сервиса в категории и сериализатор для детальной информации о категории.

--- pay2u/api/serializers.py
+++ pay2u/api/serializers.py
@@ -1,10 +1,42 @@
 from rest_framework import serializers
 
-from services.models import Category
+from services.models import Category, Service
 
 
-class CategorySerializer(serializers.ModelSerializer):
-    """Сериализатор для модели Категории."""
+class ServiceSerializer(serializers.ModelSerializer):
+    """Сериализатор для модели Сервиса."""
+    class Meta:
+        model = Service
+        fields = (
+            'name',
+            'short_description',
+            'description',
+            'icon',
+            'url'
+        )
+
+
+class CategoryListSerializer(serializers.ModelSerializer):
+    """Сериализатор для списка категорий."""
     class Meta:
         model = Category
-        fields = ('name', 'slug')
+        fields = (
+            'name',
+            'slug'
+        )
+
+
+class CategoryDetailSerializer(serializers.ModelSerializer):
+    """Сериализатор для детальной информации о категории."""
+    services = ServiceSerializer(
+        many=True,
+        read_only=True
+    )
+
+    class Meta:
+        model = Category
+        fields = (
+            'name',
+            'slug',
+            'services'
+        )
