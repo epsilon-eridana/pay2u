2cbffe321f58b909ea367bffe365df161724a41d
andrey <mist3s@github.com>
2024-04-06 14:05:54 +0200

    Написать условие использование сериализаторов в зависимости от действия.

--- pay2u/api/views.py
+++ pay2u/api/views.py
@@ -1,10 +1,14 @@
 from rest_framework import status, viewsets
 
-from .serializers import CategorySerializer
+from .serializers import CategoryListSerializer, CategoryDetailSerializer
 from services.models import Category
 
 
 class CategoryViewSet(viewsets.ReadOnlyModelViewSet):
-    """Вьюсет для модели категории."""
     queryset = Category.objects.all()
-    serializer_class = CategorySerializer
+    serializer_class = CategoryListSerializer
+
+    def get_serializer_class(self):
+        if self.action == 'retrieve':
+            return CategoryDetailSerializer
+        return self.serializer_class
